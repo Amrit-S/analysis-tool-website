cbf0cb58fc0bf943bf8d7718797fec12dddee7fc
Amrit Kaur Singh <42155577+Amrit-S@users.noreply.github.com>
2021-02-26 17:15:38 -0800

    Feature/amrit s/file dropbox (#7)
    * Dictionaries - delete bug
    
    * Working
    
    * Finally working - fixed deletion bug
    
    * Synced to parent + fixed deletion bug
    
    * Added scroll and natural sort
    
    * Added comments + fixed css
    
    * Added some comments to AnalysisInput for usage
    
    * Limited always shown scrollbar to Dropbox files

--- frontend/src/pages/AnalysisInput.js
+++ frontend/src/pages/AnalysisInput.js
@@ -1,16 +1,44 @@
-import React, { Component} from 'react';
+import React, { Component, useCallback} from 'react';
+import { Button, Snackbar } from '@material-ui/core';
 
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 
-const config = require('../config');
+import DropBox from '../components/DropBox';
 
+const config = require('../config');
 //const BACKEND_URL = config.backend.uri;
 
-class AnalysisInput extends Component {
+export default function AnalysisInput() {
 
-    render (){
+    const [state, setState] = React.useState({  
 
+        inputFiles: []  // all file objects in dropbox can be accessed here, organized in natural sort
+    });
+
+    /**
+     * Allows the state atribute inputFiles to be updated to reflect changes made in the DropBox component. 
+     */
+    const setFiles = useCallback(
+        (files) => {
+          setState({inputFiles: files});
+        },
+        [], // Tells React to memoize regardless of arguments.
+      );
+    
+    /**
+     * Very simple dummy function to verify state has most updated files from DropBox. 
+     */
+    function getFiles(){
+
+        let x = "";
+        for(var i=0; i < state.inputFiles.length; i++){
+            x += " " + state.inputFiles[i].name;
+        }
+
+        alert(x);
+    }
+        
       return (
 
           <div>
@@ -18,11 +46,12 @@ class AnalysisInput extends Component {
               <div style={{marginTop: "30px"}}>
                   This is the Data Retrieval Page for Analysis.
               </div>
+              <DropBox handleFiles={setFiles}/>
+              <button onClick={getFiles}>Parent Files</button>
               <Footer/>
           </div>
 
       )
-    }
   }
   
-  export default AnalysisInput;
+//   export default AnalysisInput;
