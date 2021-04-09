843509bb9e5dd66d94eacf7db1ac06323097ddb6
Amrit-S <aksingh@ucsd.edu>
2021-04-09 03:54:47 -0700

    Most of page is done, working on adding props

--- frontend/src/pages/AnalysisInput.js
+++ frontend/src/pages/AnalysisInput.js
@@ -1,15 +1,35 @@
-import React, { Component, useCallback} from 'react';
-import { Button, Snackbar } from '@material-ui/core';
+import React, {useCallback} from 'react';
 
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-
+import InputInstructions from '../components/InputInstructions';
 import DropBox from '../components/DropBox';
+import CustomizeSettingsDropDown from "../components/CustomizeSettingsDropDown";
+import { Button, Snackbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import "../css/AnalysisInput.css";
 
 const config = require('../config');
 //const BACKEND_URL = config.backend.uri;
 
 export default function AnalysisInput() {
+
+    const useStyles = makeStyles((theme) => ({
+        button: {
+          "& .MuiButton-root": {
+            margin: theme.spacing(3),
+            color: "black",
+            background: "#DABA11",
+            padding: "10px 40px",
+            fontSize: "16px",
+            border: "1px solid black",
+            // width: "30%"
+          },
+        },
+      }));
+
+      const classes = useStyles();
 
     const [state, setState] = React.useState({  
 
@@ -38,13 +58,57 @@ export default function AnalysisInput() {
 
         alert(x);
     }
+
+
         
       return (
 
           <div>
               <NavBar/>
-              <DropBox handleFiles={setFiles}/>
-              <button onClick={getFiles}>Parent Files</button>
+              <p className="Subsection-Step-Title"> Step #1: Upload Images </p>
+              <section className="Step-1-Container">
+                  <InputInstructions/>
+                  <DropBox handleFiles={setFiles}/>
+              </section>
+              <p className="Subsection-Step-Title"> Step #2: Customize Settings </p>
+              <section className="Step-2-Container">
+                  <CustomizeSettingsDropDown title="Individual Image Analysis" info="Analysis will be conducted on each individual image seperately."
+                  options={[ 'Oliver Hansen',
+                  'Van Henry',
+                  'April Tucker',
+                  'Ralph Hubbard',
+                  'Omar Alexander',
+                  'Carlos Abbott',
+                  'Miriam Wagner',
+                  'Bradley Wilkerson',
+                  'Virginia Andrews',
+                  'Kelly Snyder']}
+                  />
+                  <div class="vl"></div>
+                  <CustomizeSettingsDropDown title="Group Image Analysis" info="Analysis will be conducted on all images holistically."
+                  options={[ 'Oliver Hansen',
+                  'Van Henry',
+                  'April Tucker',
+                  'Ralph Hubbard',
+                  'Omar Alexander',
+                  'Carlos Abbott',
+                  'Miriam Wagner',
+                  'Bradley Wilkerson',
+                  'Virginia Andrews',
+                  'Kelly Snyder']}
+                  />
+              </section>
+              {/* <button onClick={getFiles}>Parent Files</button> */}
+              <div className={`${classes.button} Submit-Button`} >
+                <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        // disabled={state.form_disabled}
+                    >
+                        Begin Analysis
+                </Button>
+              </div>
               <Footer/>
           </div>
 
