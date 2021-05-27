92ddf72d4566b42592e1a6266e08be624952802c
Levente Horvath <developer@leventehorvath.com>
2021-05-27 16:21:40 -0700

    Added download all option

--- frontend/src/components/IndividualResults.js
+++ frontend/src/components/IndividualResults.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Promise from 'bluebird';
+import JsZip from 'jszip';
+import FileSaver from 'file-saver';
 
 import IndividualResultRow from "../components/IndividualResultRow";
 import {ANALYSIS_OPTIONS} from "../constants/analysisOptions";
+import '../css/IndividualResults.css';
 
 export default function IndividualResults(props) {
 
@@ -21,10 +25,92 @@ export default function IndividualResults(props) {
         };
         return options;
     }
+
+    async function downloadAll() {
+        // collect csv backend calls for all images
+        let downloadCalls = [];
+        for (const element in seg) {
+            if (Object.hasOwnProperty.call(seg, element)) {
+                const image = seg[element];
+                const res = {
+                    "data": 
+                        {
+                            "stats": {
+                                "size": image.stats.size.data,
+                                "shape": image.stats.shape.data,
+                                "pointiness": image.stats.pointiness.data
+                            },
+                            "totalCells": image.stats.size.totalCells
+                        }
+                    };
+        
+                    downloadCalls.push( fetch('/segmentation/download', {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json'},
+                        responseType: 'text/csv',
+                        body: JSON.stringify(res)
+                    }))
+            }
+        }
+
+        // create downloadable URL for each returned csv
+        const results = await Promise.all(downloadCalls);
+        let urls = [];
+        for (const element in results) {
+            if (Object.hasOwnProperty.call(results, element)) {
+                const res = results[element];
+                // successful response
+                if(res.status === 200){
+                    // get csv file, and make it browser readable 
+                    var data = new Blob([await res.blob()], {type: 'text/csv'});
+                    var csvURL = window.URL.createObjectURL(data);
+                    urls.push(csvURL);
+                }
+            }
+        }
+
+        downloadAndZip(urls, urls.length);
+    }
+      
+
+    // the following functions enable zipping multiple files and downloading the zip
+    // (source: https://huynvk.dev/blog/download-files-and-zip-them-in-your-browsers-using-javascript)
+
+    const download = url => {
+        return fetch(url).then(resp => resp.blob());
+    };
+    
+    const downloadByGroup = (urls, files_per_group=5) => {
+        return Promise.map(
+            urls, 
+            async url => {
+            return await download(url);
+            },
+            {concurrency: files_per_group}
+        );
+    }
+    
+    const exportZip = blobs => {
+        const zip = JsZip();
+        blobs.forEach((blob, i) => {
+            zip.file(props.inputPageData.inputFileJSONs[i].name + ".csv", blob);
+        });
+        zip.generateAsync({type: 'blob'}).then(zipFile => {
+            const fileName = `Segmentation_Data.zip`;
+            return FileSaver.saveAs(zipFile, fileName);
+        });
+    }
+    
+    const downloadAndZip = (urls, num) => {
+        return downloadByGroup(urls, num).then(exportZip);
+    }  
     
     return (
 
         <>
+        <div className="Download-All">
+            <button className="Download" title="Download full data" onClick={downloadAll}> Download All ↓ </button>
+        </div>
         {
         props.inputPageData.inputFileJSONs.map( (data, i) => {
             return <IndividualResultRow 
