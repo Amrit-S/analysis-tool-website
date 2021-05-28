955430a8be2310109071856af38df7a56fafc6df
Amrit-S <aksingh@ucsd.edu>
2021-05-28 16:38:43 -0700

    404 page

--- frontend/src/pages/Custom404.js
+++ frontend/src/pages/Custom404.js
@@ -1,4 +1,7 @@
 import React, { Component} from 'react';
+import InsanityMeme from '../media/404_Insanity_Meme.png';
+
+import "../css/Custom404.css";
 
 class Custom404 extends Component {
 
@@ -6,9 +9,15 @@ class Custom404 extends Component {
 
       return (
 
-          <div>
-              <div style={{marginTop: "30px"}}>
-                  Custom 404 page because CSE134B says its a thing :)
+          <div className="Custom-404">
+              <div className="meme">
+                 <img src={InsanityMeme} alt="Albert Einstein deems machine learning as insanity."></img>
+              </div>
+              <div className="message">
+                <h1> 404 </h1>
+                <p>
+                    Uh oh, seems like this page doesn't exist. Try clicking on a page on the Navigation Bar; otherwise enjoy the meme. 
+                </p>
               </div>
           </div>
 
